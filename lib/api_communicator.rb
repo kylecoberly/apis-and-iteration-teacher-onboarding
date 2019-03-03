d4bea740658635f1268cc79fb37dda9e4535a113
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 21:44:23 +0000

    Automatically backed up by Learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -11,17 +11,6 @@ def get_character_movies_from_api(character)
     RestClient.get(endpoint)
   })
   get_names_from_endpoints(films)
-
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
 end
 
 def get_names_from_endpoints(film_endpoints)
