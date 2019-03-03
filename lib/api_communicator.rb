76a19c6b8aa5eddbd1800a479c8b5982a91d0185
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 21:45:37 +0000

    Automatically backed up by Learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,7 +3,6 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
   film_endpoints = reponse_hash["results"].first["films"]
