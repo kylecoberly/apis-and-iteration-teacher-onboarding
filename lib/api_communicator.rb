32698a7320bbd5eade69c333abfcafeb70533c9a
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 21:37:21 +0000

    Automatically backed up by Learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,10 +6,7 @@ def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
-
+  film_endpoints = reponse_hash["results"].first.["films"]
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
